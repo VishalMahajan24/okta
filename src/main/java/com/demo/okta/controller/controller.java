38ef50f6b06820ec58f0f0162c92e8755d569109
VishalMahajan24 <113411277+VishalMahajan24@users.noreply.github.com>
2024-04-22 11:21:02 +0530

    The okta commit

---
+++ src/main/java/com/demo/okta/controller/controller.java
@@ -0,0 +1,15 @@
+package com.demo.okta.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@RequestMapping("/hello")
+	
+	public String hey (){
+		return "hello World";
+	}
+
+}
